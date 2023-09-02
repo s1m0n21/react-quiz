0106f8336b4de0d325754472aca1e9b5cf1d6903
s1m0n21 <me@s1m0n21.com>
2023-09-02 19:26:57 +0800

    add husky

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    jsxBracketSameLine: false,
+    printWidth: 100,
+    proseWrap: 'preserve',
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    parser: 'typescript',
+  }
